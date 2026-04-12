7b7e33d0292f6fe7210e6ad0b135e655803270ec
mahmuterkanyesil <mahmuterkanyesil@gmail.com>
2026-04-12 18:45:45 +0300

    feat: api created

---
+++ cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"subsync/internal/core/application/service"
+	"subsync/internal/infrastructure/adapter/persistence/sqlite"
+	"subsync/internal/infrastructure/adapter/queue/asynq"
+	"subsync/internal/infrastructure/adapter/rest/gin"
+	"subsync/pkg/config"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	cfg := config.Load()
+
+	db, err := sql.Open("sqlite", cfg.StateDBPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	subtitleRepo := sqlite.NewSQLiteSubtitleRepository(db)
+	apiKeyRepo := sqlite.NewSQLiteAPIKeyRepository(db)
+	taskQueue := asynq.NewAsynqTaskQueue(cfg.RedisURL)
+
+	statsService := service.NewStatsService(subtitleRepo, apiKeyRepo, taskQueue)
+
+	server := gin.NewHTTPServer(statsService, cfg.APIPort)
+
+	log.Println("api started on port", cfg.APIPort)
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
