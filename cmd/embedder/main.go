fef25427bf626fff54040f3a85f942acb3efc29a
mahmuterkanyesil <mahmuterkanyesil@gmail.com>
2026-04-12 18:41:00 +0300

    feat: embedder created

---
+++ cmd/embedder/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	"subsync/internal/core/application/service"
+	"subsync/internal/infrastructure/adapter/persistence/sqlite"
+	"subsync/internal/infrastructure/adapter/video/ffmpeg"
+	"subsync/pkg/config"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	cfg := config.Load()
+
+	db, err := sql.Open("sqlite", cfg.StateDBPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	subtitleRepo := sqlite.NewSQLiteSubtitleRepository(db)
+	videoProcessor := ffmpeg.NewFFmpegProcessor()
+
+	embeddingService := service.NewEmbeddingService(subtitleRepo, videoProcessor)
+
+	log.Println("embedder started")
+	for {
+		if err := embeddingService.EmbedPending(context.Background()); err != nil {
+			log.Printf("embed error: %v", err)
+		}
+		time.Sleep(time.Duration(cfg.EmbedIntervalSec) * time.Second)
+	}
+}
